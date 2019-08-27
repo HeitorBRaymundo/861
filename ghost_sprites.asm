d6dc1c852193aac17af3cd360471f9864e65a45f
Luiz Cartolano <duducartolano@gmail.com>
2019-08-27 12:22:48 -0300

    adding the firs 8x8 part of the ghost, now we have to extend to the rest

---
+++ ghost_sprites.asm
@@ -0,0 +1,20 @@
+SpriteGhostUpRight:
+    .db %00000000
+    .db %11000000
+    .db %11110000
+    .db %11111000
+    .db %11111100
+    .db %11011100
+    .db %11111100
+    .db %11111100
+    ; less significant bit
+
+    .db %11000000
+    .db %00110000
+    .db %00001000
+    .db %01100100
+    .db %01100010
+    .db %01100010
+    .db %00000010
+    .db %00000011
+    ; most significant bits
