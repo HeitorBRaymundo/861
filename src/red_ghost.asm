3bc7bf9ebeb2d1bc6492a7887799f0192ff1355a
igoromote <igor.omote@gmail.com>
2019-08-28 21:11:05 -0300

    Logic expanded to all ghosts

--- src/red_ghost.asm
+++ src/red_ghost.asm
@@ -22,35 +22,59 @@ ghost4_movement:  ;Red
     BEQ Ghost4Right
 
 Ghost4Up:   ;Red
+    LDX #0
+movementLoopRedUp:
     DEC $240
     DEC $244
     DEC $248
     DEC $24C
     JSR CheckCollisionF4P
+    CLC
+    INX
+    CPX ghostSpeed
+    BNE movementLoopRedUp
     JMP End
 
 Ghost4Down:    ;Red
+    LDX #0
+movementLoopRedDown:
     INC $240
     INC $244
     INC $248
     INC $24C
     JSR CheckCollisionF4P
+    CLC
+    INX
+    CPX ghostSpeed
+    BNE movementLoopRedDown
     JMP End
 
 Ghost4Left: ;Red
+    LDX #0
+movementLoopRedLeft:
     DEC $243
     DEC $247
     DEC $24B
     DEC $24F
     JSR CheckCollisionF4P
+    CLC
+    INX
+    CPX ghostSpeed
+    BNE movementLoopRedLeft
     JMP End
 
 Ghost4Right: ;Red
+    LDX #0
+movementLoopRedRight:
     INC $243
     INC $247
     INC $24B
     INC $24F
     JSR CheckCollisionF4P
+    CLC
+    INX
+    CPX ghostSpeed
+    BNE movementLoopRedRight
     JMP End
 
 CheckCollisionF4P:
