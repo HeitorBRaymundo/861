2c7117c5a9f0dd1e526d3c8a9125fe39efdbfea3
Luiz Cartolano <duducartolano@gmail.com>
2019-08-30 09:56:49 -0300

    infinite sound

---
+++ src/sound.asm
@@ -0,0 +1,115 @@
+Sound_Load:
+    lda sfx_id
+    asl a
+    tay
+
+    lda table_sfx, y
+    sta sfx_address
+
+    lda table_sfx+1, y
+    sta sfx_address+1
+
+    ; set playing flag
+    lda #$01
+    sta sfx_playing
+
+    ; reset index and counter
+    lda #$00
+    sta sfx_index
+    sta sfx_frame
+    rts
+
+Sound_PlayFrame:
+    ; don't advance if disabled
+    lda sfx_disabled
+    bne @done
+
+    ; don't advance if not playing
+    lda sfx_playing
+    beq @done
+
+    ; update one every X frames
+    inc sfx_frame
+    lda sfx_frame
+    cmp #$06
+    bne @done
+
+    ldy sfx_index
+    lda (sfx_address), y
+
+    ; $FE = "no change" or just keep playing the last note.
+    cmp #$FE
+    beq @cont
+
+    ; data is $FF terminated
+    cmp #$FF
+    bne @note
+
+    ; stop sound and return
+    lda #$30
+    sta $4000
+
+    lda #$00
+    sta sfx_playing
+    sta sfx_frame
+    rts
+
+@note:
+    ; get index (mult by two; table is a list of words)
+    asl a
+    tay
+
+    ; low byte
+    lda note_table, y
+    sta $4002
+
+    ; high byte
+    lda note_table+1, y
+    sta $4003
+
+    ; ducy cycle 01; volume F
+    lda #$7F
+    sta $4000
+
+    ; set negate flag so low squares aren't silenced
+    lda #$08
+    sta $4001
+
+@cont:
+    inc sfx_index
+    lda #0
+    sta sfx_frame
+
+@done:
+    rts
+
+table_sfx:
+    .word sfx_test,       sfx_pause,      sfx_bounce,         sfx_score
+    .word sfx_gameOver,   sfx_countdown,  sfx_countdownStart, sfx_title
+
+sfx_test:   ; sound test
+    ;    $0F,$11
+    .byte C3, D3, Ds3, G3, C4, D4, Ds4, G4
+    .byte C5, D5, Ds5, G5, C6, D6, Ds6, G6, C7, $FF     ;Cm/9
+
+sfx_pause:
+    ;    $3F,$3A
+    .byte C7, G6, C7, G6, $FF
+
+sfx_bounce:
+    .byte C2, $FF
+
+sfx_score:
+    .byte C4, D4, Ds4, $FF
+
+sfx_gameOver:
+    .byte C5, $FE, D5, $FE, Ds5, $FE, $FF
+
+sfx_countdown:
+    .byte D5, $FE, $FF
+
+sfx_countdownStart:
+    .byte G5, $FE, $FE, $FE, $FF
+
+sfx_title:
+    .byte C3, $FF
