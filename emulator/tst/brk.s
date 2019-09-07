7b095d3e5b80844186baba7ff7aa0b3aa13fc760
Igor Mateus Omote <ra169819@kiss.lab.ic.unicamp.br>
2019-09-06 21:37:41 -0300

    Makefile

--- emulator/tst/brk.s
+++ emulator/tst/brk.s
@@ -44,8 +44,8 @@ MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
    .base $10000-(PRG_COUNT*$4000)
 
 Reset:
-   brk ; Abort execution
-
+   asl $130 ; Abort execution
+   iny 
 NMI:
 
    ;NOTE: NMI code goes here
