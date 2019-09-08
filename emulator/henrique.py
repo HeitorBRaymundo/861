656087573deadf704dc45875fa018fc8d3824b96
Henrique Fuschini <henrique.fuschini@dentrodahistoria.com.br>
2019-09-08 11:31:25 -0300

    started function definition in separated file

---
+++ emulator/henrique.py
@@ -0,0 +1,97 @@
+####################################
+#  JUMP / BRANCH / RETURN / BREAK  #
+####################################
+
+def BRK():
+    print("BRK impl")
+
+def BPL():
+    print("BPL rel")
+
+def JSR():
+    print("")
+
+def BMI():
+    print("")
+
+def BVC():
+    print("")
+
+def RTS():
+    print("")
+
+def BVS():
+    print("")
+
+def BCC():
+    print("")
+
+def BCS():
+    print("")
+
+def BNE():
+    print("")
+
+def BEQ():
+    print("")
+
+def JMP_abs():
+    print("")
+
+def JMP_ind():
+    print("")
+
+
+################################
+#        FLAGS / STACK         #
+################################
+
+def PHP():
+    print("")
+
+def CLC():
+    print("")
+
+def PLP():
+    print("")
+
+def SEC():
+    print("")
+
+def PHA():
+    print("")
+
+def PLA():
+    print("")
+
+def CLV():
+    print("")
+
+def CLD():
+    print("")
+
+def SED():
+    print("")
+
+
+
+################################
+#        BIT OPERATIONS        #
+################################
+
+def BIT():
+    print("")
+
+def BIT_abs():
+    print("")
+
+
+################################
+#         INTERUPTIONS         #
+################################
+
+def RTI():
+    print("")
+
+def CLI():
+    print("")
