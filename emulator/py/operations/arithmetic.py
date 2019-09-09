516cfaa229e88d3963482af477f91b62471e9251
igoromote <igor.omote@gmail.com>
2019-09-08 22:15:32 -0300

    Arithmetic general class with one example

--- emulator/py/operations/arithmetic.py
+++ emulator/py/operations/arithmetic.py
@@ -1,12 +1,19 @@
 class Arithmetic():
-  reg = ''
-  index = 0
-  use_carry = False
-  is_zero_page = False
-  absolute = 0
-  def __init__(self):
-      self.reg = reg
-      self.index = index
-      self.use_carry = use_carry
-      self.is_zero_page = is_zero_page
-      self.absolute = absolute
+    is_carry_set = False
+    value_A = 0
+    value_second = 0
+    operation = ''
+    def __init__(self, is_carry_set: Boolean, value_A: int, value_second: int):
+        self.is_carry_set = is_carry_set
+        self.value_A = value_A
+        self.value_second = value_second
+
+    def execute():
+        # A + M + C -> A, C
+        # checar se teve overflow
+        # atualizar valor de A
+        print (self.operation)
+
+class AddWithCarry0x61(Arithmetic):
+    def __init__(self, carry: Boolean, X: str, index: int, value_A: int):
+        super().init(self, carry, valueA, X[index])
