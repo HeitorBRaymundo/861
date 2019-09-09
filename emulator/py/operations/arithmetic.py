2c9ef570207b1ec97a622ab821392fc6e59c1f03
igoromote <igor.omote@gmail.com>
2019-09-08 21:52:45 -0300

    General class made

---
+++ emulator/py/operations/arithmetic.py
@@ -0,0 +1,12 @@
+class Arithmetic():
+  reg = ''
+  index = 0
+  use_carry = False
+  is_zero_page = False
+  absolute = 0
+  def __init__(self):
+      self.reg = reg
+      self.index = index
+      self.use_carry = use_carry
+      self.is_zero_page = is_zero_page
+      self.absolute = absolute
