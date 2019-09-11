281b5d4b7c3e4071a05e28bdd0ef54e49e7bbd39
igoromote <igor.omote@gmail.com>
2019-09-11 19:52:44 -0300

    ROL done

--- emulator/py/operations/shift.py
+++ emulator/py/operations/shift.py
@@ -27,4 +27,35 @@ class ASL_zero_page_index_0x16(ASL_Op):
 
 class ASL_abs_X_0x01E(ASL_Op):
     def __init__(self, abs: int, X: int, operation: str):
-        super().init(self, zpg_index, "Op 1E")
+        super().init(self, abs[X], "Op 1E")
+
+class ROL_Op():
+    operation = ''
+    position = 0
+    def __init__(self, position: int, operation: str):
+        self.position = position
+        self.operation = operation
+
+    def execute():
+        #  C <- [76543210] <- C
+        print (self.operation)
+
+class ROL_zero_page_0x26(ASL_Op):
+    def __init__(self, zpg_index: int, operation: str):
+        super().init(self, zpg_index, "Op 26")
+
+class ROL_A_0x2A(ASL_Op):
+    def __init__(self, value_A: int, operation: str):
+        super().init(self, value_A, "Op 2A")
+
+class ROL_absolute_0x2E(ASL_Op):
+    def __init__(self, abs: int, operation: str):
+        super().init(self, abs, "Op 2E")
+
+class ROL_zero_page_index_0x36(ASL_Op):
+    def __init__(self, zpg_index: int, X: int, operation: str):
+        super().init(self, zpg_index[X], "Op 36")
+
+class ROL_abs_X_0x03E(ASL_Op):
+    def __init__(self, abs: int, X: int, operation: str):
+        super().init(self, abs[X], "Op 3E")
