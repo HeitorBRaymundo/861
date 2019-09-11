e25b40647eb7a03cbcbf37b584edb06cadb8cf71
igoromote <igor.omote@gmail.com>
2019-09-11 19:57:09 -0300

    ASR and ROR done

--- emulator/py/operations/shift.py
+++ emulator/py/operations/shift.py
@@ -40,22 +40,54 @@ class ROL_Op():
         #  C <- [76543210] <- C
         print (self.operation)
 
-class ROL_zero_page_0x26(ASL_Op):
+class ROL_zero_page_0x26(ROL_Op):
     def __init__(self, zpg_index: int, operation: str):
         super().init(self, zpg_index, "Op 26")
 
-class ROL_A_0x2A(ASL_Op):
+class ROL_A_0x2A(ROL_Op):
     def __init__(self, value_A: int, operation: str):
         super().init(self, value_A, "Op 2A")
 
-class ROL_absolute_0x2E(ASL_Op):
+class ROL_absolute_0x2E(ROL_Op):
     def __init__(self, abs: int, operation: str):
         super().init(self, abs, "Op 2E")
 
-class ROL_zero_page_index_0x36(ASL_Op):
+class ROL_zero_page_index_0x36(ROL_Op):
     def __init__(self, zpg_index: int, X: int, operation: str):
         super().init(self, zpg_index[X], "Op 36")
 
-class ROL_abs_X_0x03E(ASL_Op):
+class ROL_abs_X_0x03E(ROL_Op):
     def __init__(self, abs: int, X: int, operation: str):
         super().init(self, abs[X], "Op 3E")
+
+
+class LSR_Op():
+    operation = ''
+    position = 0
+    def __init__(self, position: int, operation: str):
+        self.position = position
+        self.operation = operation
+
+    def execute():
+        #   0 -> [76543210] -> C 
+        print (self.operation)
+
+class LSR_zero_page_0x46(LSR_Op):
+    def __init__(self, zpg_index: int, operation: str):
+        super().init(self, zpg_index, "Op 46")
+
+class LSR_A_0x4A(LSR_Op):
+    def __init__(self, value_A: int, operation: str):
+        super().init(self, value_A, "Op 4A")
+
+class LSR_absolute_0x4E(LSR_Op):
+    def __init__(self, abs: int, operation: str):
+        super().init(self, abs, "Op 4E")
+
+class LSR_zero_page_index_0x56(LSR_Op):
+    def __init__(self, zpg_index: int, X: int, operation: str):
+        super().init(self, zpg_index[X], "Op 56")
+
+class LSR_abs_X_0x05E(LSR_Op):
+    def __init__(self, abs: int, X: int, operation: str):
+        super().init(self, abs[X], "Op 5E")
