3ef7238801c065d3125f6e7ef02f3badb86d3e5f
Henrique Fuschini <henrique.fuschini@dentrodahistoria.com.br>
2019-09-13 19:51:29 -0300

    Bit alterations

--- emulator/py/operations/bit.py
+++ emulator/py/operations/bit.py
@@ -3,9 +3,9 @@
 ################################
 
 class BIT_zpg0x24():
-    def __init__(self, carry: Boolean, zpg_index: int, X: int, value_A: int, operation: str):
+    def __init__(self, zpg: int, A: int, value_A: int, operation: str):
         print("BIT zpg")
 
 class BIT_abs0x2C():
-    def __init__(self, carry: Boolean, zpg_index: int, X: int, value_A: int, operation: str):
+    def __init__(self, A: int, value_A: int, operation: str):
         print("BIT abs")
