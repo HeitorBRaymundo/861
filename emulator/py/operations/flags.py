1fac892e43bb8347383bcc4e016745b61248a222
Henrique Fuschini <henrique.fuschini@dentrodahistoria.com.br>
2019-09-13 19:46:02 -0300

    Auto stash before rebase of "emulator"

---
+++ emulator/py/operations/flags.py
@@ -0,0 +1,30 @@
+################################
+#        FLAGS / STACK         #
+################################
+
+def PHP():
+    print("PHP impl")
+
+def CLC():
+    print("CLC impl")
+
+def PLP():
+    print("PLP impl")
+
+def SEC():
+    print("SEC impl")
+
+def PHA():
+    print("PHA impl")
+
+def PLA():
+    print("PLA impl")
+
+def CLV():
+    print("CLV impl")
+
+def CLD():
+    print("CLD impl")
+
+def SED():
+    print("SED impl")
