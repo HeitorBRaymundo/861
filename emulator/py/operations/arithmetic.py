19b49155298338302bc0c52b169e6d22e679898f
igoromote <igor.omote@gmail.com>
2019-09-14 11:07:49 -0300

    Fixed minor syntax errors

--- emulator/py/operations/arithmetic.py
+++ emulator/py/operations/arithmetic.py
@@ -3,7 +3,7 @@ class ADC_Op():
     value_A = 0
     value_second = 0
     operation = ''
-    def __init__(self, is_carry_set: Boolean, value_A: int, value_second: int, operation: str):
+    def __init__(self, is_carry_set: bool, value_A: int, value_second: int, operation: str):
         self.is_carry_set = is_carry_set
         self.value_A = value_A
         self.value_second = value_second
@@ -19,7 +19,7 @@ class SBC_Op():
     value_A = 0
     value_second = 0
     operation = ''
-    def __init__(self, is_carry_set: Boolean, value_A: int, value_second: int, operation: str):
+    def __init__(self, is_carry_set: bool, value_A: int, value_second: int, operation: str):
         self.is_carry_set = is_carry_set
         self.value_A = value_A
         self.value_second = value_second
@@ -31,74 +31,74 @@ class SBC_Op():
         print (self.operation)
 
 class AddWithCarry0x61(ADC_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, X[index], "Op 61")
 
 
 class AddWithCarry0x65(ADC_Op):
-    def __init__(self, carry: Boolean, zpg_index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, zpg_index: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, zpg_index, "Op 65")
 
 
 class AddWithCarry0x69(ADC_Op):
-    def __init__(self, carry: Boolean, imm: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, imm: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, imm, "Op 69")
 
 
 class AddWithCarry0x6D(ADC_Op):
-    def __init__(self, carry: Boolean, abs: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, abs: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, abs, "Op 6D")
 
 class AddWithCarry0x71(ADC_Op):
-    def __init__(self, carry: Boolean, index: int, Y: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, index: int, Y: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, Y[índex], "Op 71")
 
 class AddWithCarry0x75(ADC_Op):
-    def __init__(self, carry: Boolean, zpg_index: int, X: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, zpg_index: int, X: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, zpg_index[X], "Op 75")
 
 class AddWithCarry0x79(ADC_Op):
-    def __init__(self, carry: Boolean, abs: int, Y: int value_A: int, operation: str):
+    def __init__(self, carry: bool, abs: int, Y: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, abs[Y], "Op 79")
 
 class AddWithCarry0x7D(ADC_Op):
-    def __init__(self, carry: Boolean, abs: int, X: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, abs: int, X: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, abs[X], "Op 7D")
 
 
 
 
 class SubWithCarry0xE1(SBC_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, X[index], "Op E1")
 
 
 class SubWithCarry0xE5(SBC_Op):
-    def __init__(self, carry: Boolean, zpg_index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, zpg_index: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, zpg_index, "Op E5")
 
 
 class SubWithCarry0xE9(SBC_Op):
-    def __init__(self, carry: Boolean, imm: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, imm: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, imm, "Op E9")
 
 
 class SubWithCarry0xED(SBC_Op):
-    def __init__(self, carry: Boolean, abs: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, abs: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, abs, "Op ED")
 
 class SubWithCarry0xF1(SBC_Op):
-    def __init__(self, carry: Boolean, index: int, Y: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, index: int, Y: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, Y[índex], "Op F1")
 
 class SubWithCarry0xF5(SBC_Op):
-    def __init__(self, carry: Boolean, zpg_index: int, X: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, zpg_index: int, X: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, zpg_index[X], "Op F5")
 
 class SubWithCarry0xF9(SBC_Op):
-    def __init__(self, carry: Boolean, abs: int, Y: int value_A: int, operation: str):
+    def __init__(self, carry: bool, abs: int, Y: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, abs[Y], "Op F9")
 
 class SubWithCarry0xFD(SBC_Op):
-    def __init__(self, carry: Boolean, abs: int, X: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, abs: int, X: int, value_A: int, operation: str):
         super().__init__(self, carry, valueA, abs[X], "Op FD")
--- emulator/py/operations/compare.py
+++ emulator/py/operations/compare.py
@@ -5,7 +5,7 @@ class CMP_Op():
     value_A = 0
     value_second = 0
     operation = ''
-    def __init__(self, is_carry_set: Boolean, value_A: int, value_second: int, operation: str):
+    def __init__(self, is_carry_set: bool, value_A: int, value_second: int, operation: str):
         self.is_carry_set = is_carry_set
         self.value_A = value_A
         self.value_second = value_second
@@ -18,153 +18,153 @@ class CMP_Op():
 
 
 class OrWithAcumulator0x01(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 01")
 
 class OrWithAcumulator0x05(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 05")
 
 class OrWithAcumulator0x09(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 09")
 
 class OrWithAcumulator0x0D(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 0D")
 
 class OrWithAcumulator0x11(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 11")
 
 class OrWithAcumulator0x15(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 15")
 
 class OrWithAcumulator0x19(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 19")
 
 class OrWithAcumulator0x1D(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 1D")
 
 class AndWithAcumulator0x21(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 21")
 
 class AndWithAcumulator0x25(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 25")
 
 class AndWithAcumulator0x29(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 29")
 
 class AndWithAcumulator0x2D(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 2D")
 
 class AndWithAcumulator0x31(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 31")
 
 class AndWithAcumulator0x35(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 35")
 
 class AndWithAcumulator0x39(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 39")
 
 class AndWithAcumulator0x3D(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 3D")
 
 class ExclusiveOrWithAcumulator0x41(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 41")
 
 class ExclusiveOrWithAcumulator0x45(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 45")
 
 class ExclusiveOrWithAcumulator0x49(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 49")
 
 class ExclusiveOrWithAcumulator0x4D(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 4D")
 
 class ExclusiveOrWithAcumulator0x51(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 51")
 
 class ExclusiveOrWithAcumulator0x55(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 55")
 
 class ExclusiveOrWithAcumulator0x59(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 59")
 
 class ExclusiveOrWithAcumulator0x5D(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op 5D")
 
 class CompareWithY0xC0(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op C0")
 
 class CompareWithY0xC4(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op C4")
 
 class CompareWithY0xCC(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op CC")
 
 class CompareWithAcumulator0xC1(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op C1")
 
 class CompareWithAcumulator0xC5(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op C5")
 
 class CompareWithAcumulator0xC9(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op C9")
 
 class CompareWithAcumulator0xCD(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op CD")
 
 class CompareWithAcumulator0xD1(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op D1")
 
 class CompareWithAcumulator0xD5(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op D5")
 
 class CompareWithAcumulator0xD9(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op D9")
 
 class CompareWithAcumulator0xDD(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op DD")
 
 class CompareWithX0xE0(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op E0")
 
 class CompareWithX0xE4(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op E4")
 
 class CompareWithX0xEC(CMP_Op):
-    def __init__(self, carry: Boolean, X: str, index: int, value_A: int, operation: str):
+    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
         super().init(self, carry, valueA, X[index], "Op EC")
