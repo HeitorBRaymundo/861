ab9c0ce12fd53c38dd9533d39f45c112fa62ca5b
igoromote <igor.omote@gmail.com>
2019-09-20 19:49:20 -0300

    refactoring arithmeticc

--- emulator/py/operations/arithmetic.py
+++ emulator/py/operations/arithmetic.py
@@ -1,11 +1,8 @@
 class ADC_Op():
-    is_carry_set = False
-    value_A = 0
     value_second = 0
-    operation = ''
-    def __init__(self, is_carry_set: bool, value_A: int, value_second: int, operation: str):
-        self.is_carry_set = is_carry_set
-        self.value_A = value_A
+    system = ""
+    def __init__(self, systemCPU: System, value_second: int):
+        self.system = systemCPU
         self.value_second = value_second
 
     def execute():
@@ -15,13 +12,10 @@ class ADC_Op():
         print (self.operation)
 
 class SBC_Op():
-    is_carry_set = False
-    value_A = 0
     value_second = 0
-    operation = ''
-    def __init__(self, is_carry_set: bool, value_A: int, value_second: int, operation: str):
-        self.is_carry_set = is_carry_set
-        self.value_A = value_A
+    system = ""
+    def __init__(self, systemCPU: System, value_second: int):
+        self.system = systemCPU
         self.value_second = value_second
 
     def execute():
@@ -31,74 +25,74 @@ class SBC_Op():
         print (self.operation)
 
 class AddWithCarry0x61(ADC_Op):
-    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, X[index], "Op 61")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, SystemCPU.MEM[SystemCPU.getY()], "Op 61")
 
 
 class AddWithCarry0x65(ADC_Op):
-    def __init__(self, carry: bool, zpg_index: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, zpg_index, "Op 65")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, zpg_index, "Op 65")
 
 
 class AddWithCarry0x69(ADC_Op):
-    def __init__(self, carry: bool, imm: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, imm, "Op 69")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, imm, "Op 69")
 
 
 class AddWithCarry0x6D(ADC_Op):
-    def __init__(self, carry: bool, abs: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, abs, "Op 6D")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, abs, "Op 6D")
 
 class AddWithCarry0x71(ADC_Op):
-    def __init__(self, carry: bool, index: int, Y: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, Y[índex], "Op 71")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, Y[índex], "Op 71")
 
 class AddWithCarry0x75(ADC_Op):
-    def __init__(self, carry: bool, zpg_index: int, X: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, zpg_index[X], "Op 75")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, zpg_index[X], "Op 75")
 
 class AddWithCarry0x79(ADC_Op):
-    def __init__(self, carry: bool, abs: int, Y: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, abs[Y], "Op 79")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, abs[Y], "Op 79")
 
 class AddWithCarry0x7D(ADC_Op):
-    def __init__(self, carry: bool, abs: int, X: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, abs[X], "Op 7D")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, abs[X], "Op 7D")
 
 
 
 
 class SubWithCarry0xE1(SBC_Op):
-    def __init__(self, carry: bool, X: str, index: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, X[index], "Op E1")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, X[index], "Op E1")
 
 
 class SubWithCarry0xE5(SBC_Op):
-    def __init__(self, carry: bool, zpg_index: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, zpg_index, "Op E5")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, zpg_index, "Op E5")
 
 
 class SubWithCarry0xE9(SBC_Op):
-    def __init__(self, carry: bool, imm: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, imm, "Op E9")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, imm, "Op E9")
 
 
 class SubWithCarry0xED(SBC_Op):
-    def __init__(self, carry: bool, abs: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, abs, "Op ED")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, abs, "Op ED")
 
 class SubWithCarry0xF1(SBC_Op):
-    def __init__(self, carry: bool, index: int, Y: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, Y[índex], "Op F1")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, Y[índex], "Op F1")
 
 class SubWithCarry0xF5(SBC_Op):
-    def __init__(self, carry: bool, zpg_index: int, X: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, zpg_index[X], "Op F5")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, zpg_index[X], "Op F5")
 
 class SubWithCarry0xF9(SBC_Op):
-    def __init__(self, carry: bool, abs: int, Y: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, abs[Y], "Op F9")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, abs[Y], "Op F9")
 
 class SubWithCarry0xFD(SBC_Op):
-    def __init__(self, carry: bool, abs: int, X: int, value_A: int, operation: str):
-        super().__init__(self, carry, valueA, abs[X], "Op FD")
+    def __init__(self, SystemCPU: System):
+        super().__init__(self, abs[X], "Op FD")
