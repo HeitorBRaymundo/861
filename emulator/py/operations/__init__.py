a25f72c1a1a8b0d6cb1bc56034c75dc542dd89d9
Heitor <hbanhete@gmail.com>
2019-09-24 20:28:26 -0300

    passing params to methods of ORA, AND, CMP, CPY and CPX

--- emulator/py/operations/__init__.py
+++ emulator/py/operations/__init__.py
@@ -3,7 +3,7 @@ This package is responsible for identify which opcode is and process it.
 """
 
 from py.operations.arithmetic import *
-# from py.operations.compare import *
+from py.operations.compare import *
 from py.operations.memory import *
 from py.operations.register_instructions import *
 from py.operations.shift import *
