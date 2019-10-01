9542b3c10515c436a014788e50cf163d0cd98285
igoromote <igor.omote@gmail.com>
2019-10-01 19:09:08 -0300

    Print function fixed

--- emulator/emulator.py
+++ emulator/emulator.py
@@ -758,15 +758,15 @@ while systemCPU.program_counter < len(pgr_bytes) - 6:
         pass
 
     if addr is None:
-       print ("| pc = 0x%0.4X".lower() % int(hex(systemCPU.program_counter + 0xC000), 16),\
-              " | a = 0x%0.2X".lower() % systemCPU.getA(), "| x = 0x%0.2X".lower() %  systemCPU.getX(), \
-              "| y = 0x%0.2X".lower() %  systemCPU.getY(), "| sp = 0x%0.4X".lower() %  int(systemCPU.getSP(), 16), \
-              "| p[NV-BDIZC] = ", systemCPU.printFLAG(),"|")
+       print ("| pc = 0x%0.4x" % int(hex(systemCPU.program_counter + 0xC000), 16),\
+              "| a = 0x%0.2x" % systemCPU.getA(), "| x = 0x%0.2x" %  systemCPU.getX(), \
+              "| y = 0x%0.2x" %  systemCPU.getY(), "| sp = 0x%0.4x" %  int(systemCPU.getSP(), 16), \
+              "| p[NV-BDIZC] =", systemCPU.printFLAG(),"|")
     else:
-       print ("| pc = 0x%0.4X".lower() % int(hex(systemCPU.program_counter + 0xC000), 16),\
-              " | a = 0x%0.2X".lower() % systemCPU.getA(), \
-              "| x = 0x%0.2X".lower() %  systemCPU.getX(), \
-              "| y = 0x%0.2X".lower() %  systemCPU.getY(), \
-              "| sp = 0x%0.4X".lower() %  int(systemCPU.getSP(), 16), \
-              "| p[NV-BDIZC] = ", systemCPU.printFLAG(),\
-              "| MEM[0x%0.4X]" % addr, " = 0x%0.2X".lower() % systemCPU.loadMem(addr),"|")
+       print ("| pc = 0x%0.4x" % int(hex(systemCPU.program_counter + 0xC000), 16),\
+              "| a = 0x%0.2x" % systemCPU.getA(), \
+              "| x = 0x%0.2x" %  systemCPU.getX(), \
+              "| y = 0x%0.2x" %  systemCPU.getY(), \
+              "| sp = 0x%0.4x" %  int(systemCPU.getSP(), 16), \
+              "| p[NV-BDIZC] =", systemCPU.printFLAG(),\
+              "| MEM[0x%0.4x]" % addr, "= 0x%0.2x" % systemCPU.loadMem(addr),"|")
