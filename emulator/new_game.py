09e4feb7d9614344cac6ef5975d4032629ba3c44
Heitor <hbanhete@gmail.com>
2019-10-26 12:16:01 -0300

    create parsing functions

--- emulator/new_game.py
+++ emulator/new_game.py
@@ -163,15 +163,8 @@ pixel_arr = build_color_array(pixels)
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    #
-    # ballrect = ballrect.move(speed)
-    # if ballrect.left < 0 or ballrect.right > width:
-    #     speed[0] = -speed[0]
-    # if ballrect.top < 0 or ballrect.bottom > height:
-    #     speed[1] = -speed[1]
 
     screen.fill(black)
-    # screen.blit(ball, ballrect)
     pygame.display.flip()
 
     build_sprite(screen, sprites)
